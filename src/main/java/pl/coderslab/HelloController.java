224f08087ecdb2e459da3e57e141789d3dc7bf87
arcyro <arek@arcyro.pl>
2025-08-21 12:52:17 +0200

    session

--- src/main/java/pl/coderslab/HelloController.java
+++ src/main/java/pl/coderslab/HelloController.java
@@ -1,15 +1,19 @@
 package pl.coderslab;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.WebUtils;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
     @ResponseBody
-    public String helloWorld() {
+    public String helloWorld(HttpServletRequest request) {
+        Cookie c = WebUtils.getCookie(request, "cookie2");
         return "hello";
     }
 }
